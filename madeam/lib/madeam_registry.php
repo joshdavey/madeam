577b00221b5b518c3f72f8012da369b1d69634f9
joshdavey <joshdavey@4e7df5bf-5837-0410-b7c2-fd8623b19026>
2007-10-21 22:19:07 +0000

    Added comments to madeam_registry
    git-svn-id: http://madeam.googlecode.com/svn/trunk@32 4e7df5bf-5837-0410-b7c2-fd8623b19026

--- madeam/lib/madeam_registry.php
+++ madeam/lib/madeam_registry.php
@@ -1,11 +1,42 @@
 <?php
+/**
+ * Madeam :  Rapid Development MVC Framework <http://www.madeam.com/>
+ * Copyright (c)	2006, Joshua Davey
+ *								24 Ridley Gardens, Toronto, Ontario, Canada
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright		Copyright (c) 2006, Joshua Davey
+ * @link				http://www.madeam.com
+ * @package			madeam
+ * @version			0.0.5
+ * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @author      Joshua Davey
+ */
 class madeam_registry {
 
+  /**
+   * The registry is where all the data is stored to be available globally.
+   *
+   * @var array
+   */
 	public $registry;
 
+	/**
+	 * Stores this registry's instance
+	 *
+	 * @var boolean/madeam_registry object
+	 */
 	private static $_instance = false;
 
 
+	/**
+	 * Creates a new instance of the registry if it does not exist.
+	 * If it does exist then it returns the already existing instance.
+	 *
+	 * @return madeam_registry object
+	 */
   public static function instance() {
     if(!madeam_registry::$_instance) {
       madeam_registry::$_instance = new madeam_registry();
@@ -13,10 +44,23 @@ class madeam_registry {
     return madeam_registry::$_instance;
   }
 
+
+  /**
+   * Adds or modifies a an entry in the registry
+   *
+   * @param string $id
+   * @param mixed $value
+   */
 	public function set($id, $value) {
 		$this->registry[$id] = $value;
 	}
 
+	/**
+	 * Returns a registry entry by id
+	 *
+	 * @param string $id
+	 * @return mixed/boolean
+	 */
 	public function get($id) {
 		if (isset($this->registry[$id])) {
 		  return $this->registry[$id];
@@ -25,6 +69,12 @@ class madeam_registry {
 		}
 	}
 
+	/**
+	 * Checks to see if an entry exists by id
+	 *
+	 * @param string $id
+	 * @return boolean
+	 */
 	public function exists($id) {
     if (isset($this->registry[$id])) {
 		  return true;
@@ -33,6 +83,11 @@ class madeam_registry {
 		}
 	}
 
+	/**
+	 * Removes an entry in the registry by id
+	 *
+	 * @param string $id
+	 */
 	public function delete($id) {
 	  if (isset($this->registry[$id])) {
 		  unset($this->registry[$id]);
