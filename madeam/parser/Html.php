5dc20099efaea8135a8f4d946be03b1d163b3cfa
joshdavey <joshdavey@4e7df5bf-5837-0410-b7c2-fd8623b19026>
2007-12-28 07:42:51 +0000

    git-svn-id: http://madeam.googlecode.com/svn/trunk@94 4e7df5bf-5837-0410-b7c2-fd8623b19026

---
+++ madeam/parser/Html.php
@@ -0,0 +1,17 @@
+<?php
+class Parse_Html {
+	
+	public function __construct() {
+		
+	}
+	
+	public function render($view, $data) {
+		
+	}
+	
+	public function set($name, $value) {
+		
+	}
+	
+}
+?>
