3166f6ce83134daffd683ce794ddf4a8e6293fd9
joshdavey <joshdavey@4e7df5bf-5837-0410-b7c2-fd8623b19026>
2007-12-28 07:34:52 +0000

    git-svn-id: http://madeam.googlecode.com/svn/trunk@85 4e7df5bf-5837-0410-b7c2-fd8623b19026

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
+// directory splitter
+  define('DS', DIRECTORY_SEPARATOR);
+  
 // define script file name
 	define('SCRIPT_FILENAME', basename(__FILE__));
 
 // define foreign path -- necessary for when including the script from outside the public directory
-	define('FOREIGN_PATH', dirname(__FILE__));
+	define('PATH_TO_SCRIPT', dirname(__FILE__) . DS);
 
 // set public path
-	define('PUB_PATH', FOREIGN_PATH . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR);
-
+	define('PATH_TO_PUBLIC', PATH_TO_SCRIPT . 'public' . DS);
+	
 // include dispatcher in web root
-	require_once 'public/index.php';
+	require PATH_TO_PUBLIC . 'index.php';
 ?>
