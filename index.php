105e3681bcd02cf602473089b338cc8e1fe51aa8
joshdavey <joshdavey@4e7df5bf-5837-0410-b7c2-fd8623b19026>
2008-04-29 23:54:49 +0000

    - removed constants from root index.php file that points to public.index.php
    git-svn-id: http://madeam.googlecode.com/svn/trunk@160 4e7df5bf-5837-0410-b7c2-fd8623b19026

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
 <?php
-/*
-// directory splitter
-  define('DS', DIRECTORY_SEPARATOR);
-
-// define script file name
-	define('SCRIPT_FILENAME', basename(__FILE__));
-
-// define foreign path -- necessary for when including the script from outside the public directory
-	define('PATH_TO_SCRIPT', dirname(__FILE__) . DS);
-
-// set public path
-	define('PATH_TO_PUBLIC', PATH_TO_SCRIPT . 'public' . DS);
-
-// include dispatcher in web root
-	require PATH_TO_PUBLIC . 'index.php';
-*/
 	require 'public/index.php';
 ?>
