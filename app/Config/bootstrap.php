ce96d49727b613de1859daf6fec4016d4622a2f3
joshdavey <joshdavey@4e7df5bf-5837-0410-b7c2-fd8623b19026>
2008-05-06 05:21:20 +0000

    - cleaned up config's boostrap - added default template definition to app controller so new users can see where best to define it
    git-svn-id: http://madeam.googlecode.com/svn/trunk@174 4e7df5bf-5837-0410-b7c2-fd8623b19026

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -1,37 +1,24 @@
 <?php
-/**
- * Welcome to the application bootstrap file.
- */
+// Welcome to your applications bootstrap
 
-//echo date_default_timezone_get();
-date_default_timezone_set('America/Toronto');
-
-
-# uncomment to enable unicode
+// uncomment to enable unicode
 if (function_exists('mb_internal_encoding')) {
   mb_internal_encoding("UTF-8");
 }
 
 
-# uncomment to set content type to utf8
-header ('Content-type: text/html; charset=utf-8');
+// uncomment to set content type to utf8
+header('Content-type: text/html; charset=utf-8');
 
 
+// set default timezone
+date_default_timezone_set('America/Toronto');
 
-# development environment settings
-if (MADAEM_ENVIRONMENT == 'development') {
 
-  # set error reporting level
-  error_reporting(E_ALL);
+// development environment settings
+if (MADEAM_ENVIRONMENT == 'development') { error_reporting(E_ALL); }
 
-}
-
-# production environment settings
-elseif (MADEAM_ENVIRONMENT == 'production') {
-
-  error_reporting(0);
-
-}
-
+// production environment settings
+elseif (MADEAM_ENVIRONMENT == 'production') { error_reporting(0); }
 
 ?>
--- app/Controller/App.php
+++ app/Controller/App.php
@@ -1,5 +1,7 @@
 <?php
 class Controller_App extends Madeam_Controller {
-  
+
+  protected $layout = 'master';
+
 }
 ?>
