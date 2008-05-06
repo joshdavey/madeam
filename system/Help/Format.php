cc6a2b8b91353fe9c57ba8b671e8169881ea045a
joshdavey <joshdavey@4e7df5bf-5837-0410-b7c2-fd8623b19026>
2008-05-06 15:52:39 +0000

    - added number() to format helper - added money() to format helper - added static global formats to format helper for consistent site wide formats
    git-svn-id: http://madeam.googlecode.com/svn/trunk@175 4e7df5bf-5837-0410-b7c2-fd8623b19026

--- system/Help/Format.php
+++ system/Help/Format.php
@@ -1,10 +1,13 @@
 <?php
 class Help_Format {
 
-  static public $dateFormat     = 'd F o';
-  static public $datetimeFormat = 'd F o H:i:s';
-  static public $moneyFormat    = '%';
-
+  static public $dateFormat       = 'd F o';
+  static public $datetimeFormat   = 'd F o H:i:s';
+  static public $moneyFormat      = '%';
+  static public $numberThousands  = ',';
+  static public $numberDecimals   = 0;
+  static public $numberDecimal    = '.';
+  
   /**
    * Enter description here...
    *
@@ -60,8 +63,30 @@ class Help_Format {
     // set format if not defined
     if ($format === false) { $format = self::$moneyFormat; }
 
-    return money_format($format, (int) $number);
+    if (function_exists('money_format')) {
+      return money_format($format, (int) $number);
+    } else {
+      return number_format((int) $number, 2, '.', ',');
+    }
   }
 
+  /**
+   * Enter description here...
+   *
+   * @param unknown_type $number
+   * @param unknown_type $decimals
+   * @param unknown_type $decimalPoint
+   * @param unknown_type $thousands
+   * @return unknown
+   */
+  static public function number($number, $decimals = false, $decimalPoint = false, $thousands = false) {
+    // set formats if not defined
+    if ($decimals     === false)  { $decimals     = self::$numberDecimals; }
+    if ($decimalPoint === false)  { $decimalPoint = self::$numberDecimal; }
+    if ($thousands    === false)  { $thousands    = self::$numberThousands; }
+    
+    return number_format((int) $number, $decimals, $decimalPoint, $thousands);
+  }
+  
 }
 ?>
