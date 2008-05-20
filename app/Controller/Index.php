61f9b7cd16313ff8a7bf44c005dad0c1fdf15f18
joshdavey <joshdavey@4e7df5bf-5837-0410-b7c2-fd8623b19026>
2008-05-20 04:41:09 +0000

    - I keep accidently committing the index controller with my stuff in it. Anyone know how I can stop being a goof?
    git-svn-id: http://madeam.googlecode.com/svn/trunk@196 4e7df5bf-5837-0410-b7c2-fd8623b19026

--- app/Controller/Index.php
+++ app/Controller/Index.php
@@ -3,9 +3,6 @@ class Controller_Index extends Controller_App {
 
   public function index() {
     // welcome to the Index Controller's index action
-    //$this->Post->where('fail')->findAll();
-    $all = $this->Post->findAll();
-    $all = $this->Post->where('fail')->findAll();
   }
 
 }
