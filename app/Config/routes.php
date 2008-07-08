ab5e3b33b52b50a34705648a6c302f1010e38f09
joshdavey <joshdavey@4e7df5bf-5837-0410-b7c2-fd8623b19026>
2008-07-08 00:58:11 +0000

    git-svn-id: http://madeam.googlecode.com/svn/trunk@232 4e7df5bf-5837-0410-b7c2-fd8623b19026

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -7,7 +7,7 @@
  */
 
 // admin routes
-  Madeam_Router::connect(':controller/:action/:id', array('controller' => 'admin\/[^\/]+'));
+  //Madeam_Router::connect(':controller/:action/:id', array('controller' => 'admin\/[^\/]+'));
 
 // default routes
   Madeam_Router::connect(':controller/:action/:id');
