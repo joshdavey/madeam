e4a1d2a4dd461dac895824801410a122cf54c534
joshdavey <joshdavey@4e7df5bf-5837-0410-b7c2-fd8623b19026>
2008-08-06 03:02:36 +0000

    - moving mime types and browsers from setup.php into their own config files
    git-svn-id: http://madeam.googlecode.com/svn/trunk@246 4e7df5bf-5837-0410-b7c2-fd8623b19026

---
+++ app/Config/browsers.php
@@ -0,0 +1,43 @@
+<?php
+
+return array(
+	'desktop' => array(
+	  'Opera'               => 'Opera',
+	  'MSIE'				        => 'Internet Explorer',
+	  'Internet Explorer'	  => 'Internet Explorer',
+	  'Shiira'			        => 'Shiira',
+	  'Firefox'			        => 'Firefox',
+	  'Chimera'			        => 'Chimera',
+	  'Phoenix'			        => 'Phoenix',
+	  'Firebird'			      => 'Firebird',
+	  'Camino'			        => 'Camino',
+	  'Netscape'			      => 'Netscape',
+	  'OmniWeb'			        => 'OmniWeb',
+	  'Mozilla'			        => 'Mozilla',
+	  'Safari'			        => 'Safari',
+	  'Konqueror'			      => 'Konqueror',
+	  'icab'				        => 'iCab',
+	  'Lynx'				        => 'Lynx',
+	  'Links'				        => 'Links',
+	  'hotjava'			        => 'HotJava',
+	  'amaya'				        => 'Amaya',
+	  'IBrowse'			        => 'IBrowse'
+	), 
+	
+	'mobile' => array(
+		'mobileexplorer'  => 'Mobile Explorer',
+	  'openwave'			  => 'Open Wave',
+	  'opera mini'		  => 'Opera Mini',
+	  'operamini'			  => 'Opera Mini',
+	  'elaine'			    => 'Palm',
+	  'palmsource'		  => 'Palm',
+	  'digital paths'		=> 'Palm',
+	  'avantgo'			    => 'Avantgo',
+	  'xiino'				    => 'Xiino',
+	  'palmscape'			  => 'Palmscape',
+	  'nokia'				    => 'Nokia',
+	  'ericsson'			  => 'Ericsson',
+	  'blackBerry'		  => 'BlackBerry',
+	  'motorola'			  => 'Motorola'	
+	)
+);
