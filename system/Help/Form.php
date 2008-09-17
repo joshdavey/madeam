7585c52476e55905ef5f93933a0bdecc59f3913c
joshdavey <joshdavey@4e7df5bf-5837-0410-b7c2-fd8623b19026>
2008-09-17 17:42:39 +0000

    - form method defaults to post
    git-svn-id: http://madeam.googlecode.com/svn/trunk@264 4e7df5bf-5837-0410-b7c2-fd8623b19026

--- system/Help/Form.php
+++ system/Help/Form.php
@@ -23,6 +23,10 @@ class Help_Form extends Help_Html {
       $params['enctype'] = 'multipart/form-data';
     }
     
+    if (!isset($_params['method'])) {
+      $_params['method'] = 'post';
+    }
+    
     $params['action'] = Madeam::url($action);
     $params['id'] = 'form_' . Madeam_Inflector::underscorize(Madeam_Inflector::slug($params['action']));
     
