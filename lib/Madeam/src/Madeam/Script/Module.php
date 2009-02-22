66c90e9fa0328cc2d552b49900a4c2b2b210ea5a
Joshua Davey <joshdavey@gmail.com>
2009-02-22 12:01:32 -0800

    added module cli script. Just an empty template for an idea right now

---
+++ lib/Madeam/src/Madeam/Script/Module.php
@@ -0,0 +1,22 @@
+<?php
+class Script_Module extends Madeam_Console_Script {
+  
+  public function create() {
+    
+  }
+  
+  public function export() {
+    // app/conf/moduleName
+    // app/src/Controller/moduleName
+    // app/src/Model/moduleName
+    // app/src/View/moduleName
+    // public/moduleName
+    
+    /*
+    
+    \wiki\Page::findOne($id);
+    
+    */
+  }
+  
+}
