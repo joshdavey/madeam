5bf76c12f80a64737983c2537e1bcc0252d54b84
Joshua Davey <joshdavey@gmail.com>
2009-03-03 04:56:13 -0500

    madeam is now a library. You can find the madeam application in joshdavey/madeam-app on github

--- app/src/Controller/App.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Application's front controller
-class Controller_App extends Madeam_Controller {
-  
-  public function beforeFilter() {
-    $this->title = 'Powered By Madeam PHP Framework';
-  }
-
-}
--- app/src/Controller/Index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-class Controller_Index extends Controller_App {
-
-  public function indexAction() {
-    // welcome to the Index Controller's index action
-  }
-  
-}
